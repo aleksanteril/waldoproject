af866bb3fd16e79d06e70de26f82d4d7bab17837
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-20 21:17:17 +0300

    kommentoitu

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # waldoproject
 Ohjelmisto 1 kurssin ryhmäprojekti.
+
+#### Peliä varten tarvittavat packaget:
+ - mysql-connetor-python 8.0.29
+ - geopy 2.4.1
--- maingameloop/waldogame.py
+++ maingameloop/waldogame.py
@@ -1,3 +1,5 @@
+#Peliä varten täytyy asentaa mysql-connector-python 8.0.29!!!
+
 
 #Yhteyden luonti tietokantaan erillisessä filessä
 #yhteys.py kysyy tietokanta käyttäjän ja salasanan
