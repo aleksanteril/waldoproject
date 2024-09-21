6eb8ec1c3e5e6fc67a7711b79c75b05bf044ffe9
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-21 20:32:42 +0300

    sql esimerkki

--- maingameloop/waldogame.py
+++ maingameloop/waldogame.py
@@ -1,13 +1,22 @@
 #Peliä varten täytyy asentaa mysql-connector-python 8.0.29!!!
 
-
+import mysql.connector
 #Yhteyden luonti tietokantaan erillisessä filessä
-#yhteys.py kysyy tietokanta käyttäjän ja salasanan
-import yhteys
+#database.py kysyy tietokanta käyttäjän ja salasanan
+import database
+
 
 #Importataan tähän eri aliohjelmat ja kyselyaliohjelmat yms
 #Liimaillaan parhaamme mukaan ja tsemppiä :)
 
 
+#Esimerkki sql kysely! Esimerkin vuoksi miten kursori menee
+sql = (f"SELECT name "
+       f"FROM airport "
+       f"WHERE iso_country = 'FI'")
+kursori = database.yhteys.cursor()
+kursori.execute(sql)
+tulos = kursori.fetchall()
+print(tulos)
 
 #Main gameloop
