0e4223aebe323dd19a968a06ca00e7cb87593ba0
Your Name <aleksi.ruotsalo@hotmail.fi>
2024-09-21 16:04:39 +0300

    Aloitus

---
+++ aliohjelmat_jesse_aleksanteri/joku.py
@@ -0,0 +1,87 @@
+#OHJELMA:
+#STEP 1 = PELINALUSTUS
+# - PRINT PELIOHJEET
+# - PRINT KÄYTTÄJÄLLE OHJEET & KOMENTOVAIHTOEHDOT
+# - MÄÄRITÄ SIJAINTI
+# - ARVO MARKALAUKUN SIJAINTI
+# - ALOITA VIHJE LASKURI
+# - ALOITA PÄÄSTÖ LASKURI
+# - KYSY NIMI
+# - KYSY SEURAAVA MATKUSTUSMAA
+# - MATKUSTA
+
+# STEP 2
+# - ILMOITA SAAPUMISMAAN TIEDOT
+# - PÄIVITÄ SIJAINTI MATKALAUKKUUN
+# - ILMOITA PELAAJALLE = Kylmempää/lämpimämpää
+# - KM LASKURIN PÄIVITTYMINEN
+# - VIHJE LASKURIN PÄIVITTYMINEN
+# - KYSY PELAAJALTA UUSI MATKUSTUSMAA
+
+# STEP 3
+# - Luo kirjasto jossa vain sallitut matkustusmaat (eurooppa)
+# - Pelaaja saapuu automaattisesti syöttämänsä maan päälentokentälle
+
+import mysql.connector
+from geopy.distance import geodesic
+import random
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='megapint',
+         password='wine',
+         autocommit=True
+         )
+
+#  PELINALUSTUS
+def aloita_peli():
+    print("PRINT TAUSTATARINA TJPS")
+    print("PRINT TARKOITUS esim. Löydä matkalaukku niin ja näin.")
+    print("PRINT OHJEISTUS MITEN MATKUSTAA / SIIRTYY / komennot")
+    print("PRINT KIRJOITA sana ICAO -> Kirjoita maan -> saat vastauksena sen pääkentän ICAO:n ")
+
+    pelaaja = input("SYÖTÄ PELAAJAN NIMI: ")
+    current_location = "EFHK"
+    print (f"Olet lentokentälklä: {current_location}")
+    matkalaukun_sijainti = random_sijainti() # ARPOO LAUKUN SIJAINNIN
+    # MÄÄRITETÄÄN KM LASKURI JA VIHJE LASKURI
+    total_kilometrit = 0
+    vihje_counter = 0
+
+    while True:
+        user_input = input("Syötä seuraavan kentän ICAO tai kirjoita ICAO:").upper()
+
+        if user_input == "ICAO":
+            country_name = input("Syötä maan nimi: ")
+            icao_lista(country_name)
+        else:
+            if len(user_input) == 4:
+                print(f"Matkustat lentokentälle {user_input}...")
+                current_location = user_input
+            else:
+                print("Virheellinen koodi")
+
+def random_sijainti():
+    return "Placeholder"
+
+def icao_lista(country_name):
+    sql = (
+        "SELECT airport.ident AS icao_code "
+        "FROM airport "
+        "JOIN country ON airport.iso_country = country.iso_country "
+        "WHERE country.name = '" + country_name + "' AND airport.type = 'large_airport';"
+    )
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    if results:
+        print(f"Suuret lentokentät maassa {country_name}:")
+        for result in results:
+            print(f"- ICAO-koodi: {result[0]}")
+    else:
+         print(f"Ei löytynyt suuria lentokenttiä maassa: {country_name}")
+
+aloita_peli()
