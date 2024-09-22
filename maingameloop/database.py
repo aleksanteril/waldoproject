3e3abca24da5b60dda91d3b6b2e6a9d7bf790e8b
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-22 21:30:56 +0300

    sql kysely esimerkki, onko hyvä toteutus?

--- maingameloop/database.py
+++ maingameloop/database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+#Yhteyden luonti
 yhteys = mysql.connector.connect(
         host='127.0.0.1',
         port=3306,
@@ -9,8 +10,16 @@ yhteys = mysql.connector.connect(
         autocommit=True
         )
 
+
+#Yleisiä kyselyita varten, palauttaa arvon !listana jossa alkiot monikkoja!
 def database_query(query):
     kursori = yhteys.cursor()
     kursori.execute(query)
     tulos = kursori.fetchall()
     return tulos
+
+#Arvojen muuttamista varten ei palauta mitään
+def database_update(query):
+    kursori = yhteys.cursor()
+    kursori.execute(query)
+    return
