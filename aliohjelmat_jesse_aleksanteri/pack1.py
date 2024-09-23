d5d7cd45ed5ed630f76a01242e543917bafb2235
Xaruthh <jesse.mustonen@hotmail.fi>
2024-09-23 12:13:45 +0300

    lisätty komento funktio

--- aliohjelmat_jesse_aleksanteri/pack1.py
+++ aliohjelmat_jesse_aleksanteri/pack1.py
@@ -24,3 +24,43 @@ def vihje(clue):
 
 
 ################### Tähän alapuolelle JESSEN funktiot, yläpuolelle ALEKSANTERIN
+
+def user_command():
+    commands = ("vihje", "hae", "matkusta", "tracker", "lämpötila", "maat", "help")
+    user_input = None
+    while True:
+        user_input = input("Syötä seuraava liike: ").lower()
+        if user_input in commands:
+            break
+        else:
+            print("Tuntematon komento")
+    return user_input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
