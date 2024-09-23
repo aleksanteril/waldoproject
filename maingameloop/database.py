41a4e8d5f116fbf52bf73d52df84b016e797b843
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-23 14:22:49 +0300

    kehityksen ajaksi lisätty tietokannan nimen kysely

--- maingameloop/database.py
+++ maingameloop/database.py
@@ -4,7 +4,7 @@ import mysql.connector
 yhteys = mysql.connector.connect(
         host='127.0.0.1',
         port=3306,
-        database='flight_game',
+        database=input("Database NAME: "),
         user= input("Database USER: "),
         password= input("Database PASSWORD: "),
         autocommit=True
