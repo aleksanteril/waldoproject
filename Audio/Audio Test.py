38a6c5f0235556b5267b054459eeaee989e9d5c4
Xaruthh <jesse.mustonen@hotmail.fi>
2024-09-27 18:08:04 +0300

    Audio directory ja audio test.py tuotu

--- Audio/Audio Test.py
+++ Audio/Audio Test.py
@@ -1,21 +1,34 @@
+# Tuodaan playsound versio 1.2.2
 from playsound import playsound
 
-
+# Funktio joka soittaa ääniä valutin sound_num parametrin mukaan
 def play_sound(sound_num):
     if sound_num == 1:
-        playsound("C:\\Audio Files\\Radio Sound.mp3")
+        playsound("clue_sound.mp3")
         print("TOIMIIKO")
     if sound_num == 2:
-        playsound("C:\\Audio Files\\Waldo Grunt.mp3")
+        playsound("help_command.mp3")
         print("perkele")
     if sound_num == 3:
-        playsound("C:\\Audio Files\\Clue Sound.mp3")
+        playsound("radio_sound.mp3")
         print("asdasda")
     if sound_num == 4:
-        playsound("C:\\Audio Files\\Help Command.mp3")
+        playsound("right_country.mp3")
         print("Asdasd")
+    if sound_num == 5:
+        playsound("travel_sound.mp3")
+    if sound_num == 6:
+        playsound("game_start.mp3")
+    if sound_num == 7:
+        playsound("-.mp3")
+    if sound_num == 8:
+        playsound("-.mp3")
+    if sound_num == 9:
+        playsound("-.mp3")
+    if sound_num == 10:
+        playsound("-.mp3")
     return
 
-
+# Kutsuu funktiota parametrilla
 print("kgfkjgfjkgkfjkdsksfd")
 play_sound(2)
