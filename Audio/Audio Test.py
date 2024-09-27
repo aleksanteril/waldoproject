00cad5b1d6b781e2d89c0dc41e3393676d166ef3
Xaruthh <jesse.mustonen@hotmail.fi>
2024-09-27 17:05:53 +0300

    Audio directory ja audio test.py tuotu

---
+++ Audio/Audio Test.py
@@ -0,0 +1,21 @@
+from playsound import playsound
+
+
+def play_sound(sound_num):
+    if sound_num == 1:
+        playsound("C:\\Audio Files\\Radio Sound.mp3")
+        print("TOIMIIKO")
+    if sound_num == 2:
+        playsound("C:\\Audio Files\\Waldo Grunt.mp3")
+        print("perkele")
+    if sound_num == 3:
+        playsound("C:\\Audio Files\\Clue Sound.mp3")
+        print("asdasda")
+    if sound_num == 4:
+        playsound("C:\\Audio Files\\Help Command.mp3")
+        print("Asdasd")
+    return
+
+
+print("kgfkjgfjkgkfjkdsksfd")
+play_sound(2)
