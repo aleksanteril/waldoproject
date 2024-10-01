348eaee5c3f36588a6d6a122943e118719c22e56
Xaruthh <jesse.mustonen@hotmail.fi>
2024-10-01 20:30:20 +0300

    lisätty waldo äänet

--- Audio/Audio Test.py
+++ Audio/Audio Test.py
@@ -18,47 +18,47 @@ def play_game_sound(sound_num):
     elif sound_num == 7:
         playsound("command_sound.mp3")
     elif sound_num == 8:
-        playsound("simply_the_best.mp4")
+        playsound("simply_the_best.mp3")
     return
 
 # Kutsuu funktiota parametrilla
 print("Moi")
-play_game_sound(2)
+play_game_sound(1)
 
 # Erillinen funktio waldon äänille
 def play_waldo_sound(sound_num):
     if sound_num == 1:
-        playsound(".mp3")
+        playsound("w_excuse_me.mp3")
     elif sound_num == 2:
-        playsound(".mp3")
+        playsound("w_help_me.mp3")
     elif sound_num == 3:
-        playsound(".mp3")
+        playsound("w_huuh.mp3")
     elif sound_num == 4:
-        playsound(".mp3")
+        playsound("w_lets_get_going.mp3")
     elif sound_num == 5:
-        playsound(".mp3")
+        playsound("w_list_of_countries.mp3")
     elif sound_num == 6:
-        playsound(".mp3")
+        playsound("w_mmm_bad_luck.mp3")
     elif sound_num == 7:
-        playsound(".mp4")
+        playsound("w_rings_a_bell.mp3")
     elif sound_num == 8:
-        playsound(".mp3")
+        playsound("w_signals_the_same.mp3")
     elif sound_num == 9:
-        playsound(".mp3")
+        playsound("w_splendid.mp3")
     elif sound_num == 10:
-        playsound(".mp3")
+        playsound("w_top_of_the_morning.mp3")
     elif sound_num == 11:
-        playsound(".mp3")
+        playsound("w_we_getting_closer.mp3")
     elif sound_num == 12:
-        playsound(".mp3")
+        playsound("w_what_r_u_leaving_me.mp3")
     elif sound_num == 13:
-        playsound(".mp3")
+        playsound("w_where_to_next.mp3")
     elif sound_num == 14:
-        playsound(".mp3")
+        playsound("w_youre_hysterical.mp3")
     return
 
 
 
-play_waldo_sound(2)
+play_waldo_sound(13)
 
 
